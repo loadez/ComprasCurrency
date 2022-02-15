009e33481bfa02a94acf9adcce7150a44ae291bd
LoadeZ <18041717+loadez@users.noreply.github.com>
2022-02-15 19:30:29 -0300

    Atividades criadas

--- app/src/main/java/dev/loadez/comprascurrency/MainActivity.kt
+++ app/src/main/java/dev/loadez/comprascurrency/MainActivity.kt
@@ -1,11 +1,24 @@
 package dev.loadez.comprascurrency
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            val text:String = findViewById<TextView>(R.id.editTextNumber).text.toString()
+            val value: Float? = text.toFloatOrNull()
+
+            if(value!=null){
+                val intent = Intent()
+            }
+        }
     }
 }
